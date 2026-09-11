d6110d2d93840c7bb8f97898f960a3fe4b25077d
alanzhai219 <zhaixiuchuan@hotmail.com>
2026-09-11 14:40:20 +0800

    refactor: conbine context and device together

--- include/context.hpp
+++ include/context.hpp
@@ -1,11 +1,24 @@
 #pragma once
 
-#include "device.hpp"
-
 namespace llm_bricks {
 
+enum class DeviceType {
+    cpu,
+    cuda,
+    sycl,
+};
+
+enum class Backend {
+    automatic,
+    reference,
+    avx2,
+    avx512,
+    cuda,
+    sycl,
+};
+
 struct Context {
-    Device device {};
+    DeviceType device = DeviceType::cpu;
     Backend backend = Backend::automatic;
 };
 
--- src/micro/rope/rope.cpp
+++ src/micro/rope/rope.cpp
@@ -4,17 +4,20 @@
 namespace llm_bricks {
 
 Status rope(Context& context, const RopeParams& params) {
-    if (context.device.type != DeviceType::cpu) return Status::unsupported;
+    if (context.device != DeviceType::cpu) {
+        return Status::unsupported;
+    }
 
     switch (context.backend) {
-    case Backend::automatic:
-    case Backend::reference:
-        return detail::rope_ref(params);
-    case Backend::avx2:
-    case Backend::avx512:
-    case Backend::cuda:
-    case Backend::sycl:
-        return Status::unsupported;
+        case Backend::automatic:
+        case Backend::reference:
+            return detail::rope_ref(params);
+        case Backend::avx2:
+        case Backend::avx512:
+        case Backend::cuda:
+            return Status::unsupported;
+        case Backend::sycl:
+            return Status::unsupported;
     }
 
     return Status::unsupported;
